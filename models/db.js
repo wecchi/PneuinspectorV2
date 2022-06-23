64584618b50a6fbe66f62235b1e4248ddb50e669
Marcelo Wecchi <wecchi@hotmail.com>
2022-06-23 18:28:53 -0300

    review

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('sydnut8a2tojn9lr','qfwpcsgjhbk9a9fs','im8q8faxr8d7ypqn',{
+const sequelize = new Sequelize('sydnut8a2tojn9lr','userdb','passworddb',{
   host: 'cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   port:'3306',
   dialect: 'mysql'
