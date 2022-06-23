89fd81b45776fd6e22b2fae2bc8be522989ab50c
jconstancio <josecarlosjdi@gmail.com>
2022-06-23 01:20:07 -0300

    Update db2.js

--- models/db2.js
+++ models/db2.js
@@ -1,17 +1,6 @@
 
 
- const Sequelize = require('sequelize')
-const sequelize = new Sequelize('sydnut8a2tojn9lr','qfwpcsgjhbk9a9fs','im8q8faxr8d7ypqn',{
-  host: 'cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  port:'3306',
-  dialect: 'mysql'
 
- })
- 
- module.exports = {
-    Sequelize : Sequelize,
-    sequelize : sequelize
- }
 
 
  
